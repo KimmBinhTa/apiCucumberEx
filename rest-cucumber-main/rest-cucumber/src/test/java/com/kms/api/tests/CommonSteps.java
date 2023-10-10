dff5ab909d466137748fd49614df97b027d1dc87
binhta <natalieta2511@gmail.com>
2023-10-10 13:39:53 +0700

    Adding commonsteps

---
+++ rest-cucumber-main/rest-cucumber/src/test/java/com/kms/api/tests/CommonSteps.java
@@ -0,0 +1,21 @@
+package com.kms.api.tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
+
+public class CommonSteps {
+    static Response productRes;
+    public  static String path = "";
+
+    @Then("I want to verify status code returning is {int}")
+    public void i_want_to_verify_status_code_is(Integer expectedStatus) {
+        Integer actualStatus = productRes.getStatusCode();
+        org.junit.Assert.assertEquals(expectedStatus, actualStatus);
+    }
+    @Given("navigate to {string} api")
+    public void navigateToApi(String path) {
+        this.path = path;
+    }
+
+}
